7eba8daf46d91d3a2a98f63f1c92ebf8e3410470
Astakhov Sergey <fzastahov@gmail.com>
2022-08-16 01:10:10 +0300

    tree with synth

--- src/vparser.py
+++ src/vparser.py
@@ -11,17 +11,22 @@ class Project:
     
     # def build_tree(self, depth, max_depth=0)
 
-    def to_graphviz(self, root_node: str, depth: int=1, max_depth: int=0):
-        if depth == 1:
+    def to_graphviz(self, root_node: str, depth: int=0, max_depth: int=0):
+        if depth == 0:
             Project.tree = ""
 
-        Project.tree += ((depth * "  " + "|_") + root_node + "(" + self.modules[root_node]["attributes"]["src"] + ")\n")
-
-        if self.modules[root_node]["cells"] != {}:
-            for x in self.modules[root_node]["cells"].values():
-                if x['type'][0] != "$":
+        if root_node[0] != "$":
+            Project.tree += ((depth * "  " + "|_") + root_node + "(" + self.modules[root_node]["attributes"]["src"] + ")\n")
+            if self.modules[root_node]["cells"] != {}:
+                for x in self.modules[root_node]["cells"].values():
+                # if x['type'][0] != "$":
                     self.to_graphviz(x['type'], depth + 1)
                 # print(x['type'])
+        else:
+            Project.tree += ((depth * "  " + "|_") + root_node + "(synthetic)\n")
+
+
+        
 
         return Project.tree
 
