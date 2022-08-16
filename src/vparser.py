755b4ba88d033775011eb726458cbf4fbfdfd137
Astakhov Sergey <fzastahov@gmail.com>
2022-08-16 03:10:54 +0300

    subgraphs v2

--- src/vparser.py
+++ src/vparser.py
@@ -1,6 +1,6 @@
 import json
-from re import S
 import graphviz
+import uuid
 
 class Project:
 
@@ -9,34 +9,25 @@ class Project:
         self.creator = self.data["creator"] or "Undefined"
         self.modules = self.data["modules"] or {}
 
-        self.tree = ""
-        self.tree_graph = graphviz.Graph('schema_gv', filename='scheme.gv', engine='dot')
-        self.tree_graph.attr(rankdir='LR')
-    
-    # def build_tree(self, depth, max_depth=0)
-
     def to_graphviz(self, root_node: str, depth: int=0, root_graph = None, max_depth: int=0):
-        if depth == 0:
-            self.tree = ""
 
         root_graph = root_graph or graphviz.Graph('parent')
-        # root_graph.attr('graph', label=root_node)
-
-        
-            # c.node('foo'+str(depth), 'bar'+str(depth))
-            # c.edge('foo', 'bar')
+        uuid_str = str(uuid.uuid4())
 
         if root_node[0] != "$":
-            s = ((depth * "+" + "|_") + root_node + "(" + self.modules[root_node]["attributes"]["src"] + ")\n")
-            self.tree += s 
-            root_graph.node(s, s) # c.node(s, s)
-            if self.modules[root_node]["cells"] != {}:
-                for x in self.modules[root_node]["cells"].values():
+            s = ((depth * "*") + root_node + "(" + self.modules[root_node]["attributes"]["src"] + ")\n")
+            children = list(self.modules[root_node]["cells"].values())
+            if len(list(filter(lambda x: x['type'][0] != "$", children))) != 0:
+                for x in children:
                     with root_graph.subgraph(name='cluster'+root_node, node_attr={'shape': 'box'}) as c:
                         c.attr('graph', label=root_node)
                         self.to_graphviz(x['type'], depth + 1, c)
+            else: 
+                root_graph.node(s + "#" + uuid_str, s)
         else:
-            self.tree += ((depth * "+" + "|_") + root_node + "(synthetic)\n")
+            s = ((depth * "[!]") + root_node + "(synthetic)\n")
+            root_graph.node(s + "#" + uuid_str, s)
+            pass
 
         return root_graph
 
@@ -47,5 +38,4 @@ if __name__ == "__main__":
     with open(data_path, 'r') as file:
         data_txt = file.read().replace('\n', '')
     proj = Project(data_txt)
-    print(proj.to_graphviz("asic_core"))
     proj.to_graphviz("asic_core").view()
